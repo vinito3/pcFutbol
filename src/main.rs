4adb859d9ed2d04ac99f68592be9b698a724463b
alex <axel6043@gmail.com>
2023-01-24 12:17:50 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+mod window;
+use gdk::Display;
+use gtk::prelude::*;
+use gtk::{gio,gdk, Application, CssProvider, StyleContext};
+use window::Window;
+
+const APP_ID: &str = "com.pcfutbol.PcFutbol";
+
+fn main() {
+    // Register and include resources
+    gio::resources_register_include!("pcfutbol.gresource")
+        .expect("Failed to register resources.");
+
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Connect to "activate" signal of `app`
+    app.connect_startup(|_| load_css());
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run();
+}
+fn build_ui(app: &Application) {
+    // Create new window and present it
+    let window = Window::new(app);
+    window.present();
+}
+
+fn load_css() {
+    // Load the CSS file and add it to the provider
+    let provider = CssProvider::new();
+    provider.load_from_data(include_bytes!("../resources/css/style.css"));
+
+    // Add the provider to the default screen
+    StyleContext::add_provider_for_display(
+        &Display::default().expect("Could not connect to a display."),
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
+}
---
+++ src/window/imp.rs
@@ -0,0 +1,65 @@
+use glib::subclass::InitializingObject;
+use gtk::prelude::*;
+use gtk::subclass::prelude::*;
+use gtk::{glib, Button, CompositeTemplate,Image};
+
+// ANCHOR: object
+// Object holding the state
+#[derive(CompositeTemplate, Default)]
+#[template(resource = "/com/pcfutbol/resources/pcfutbol.ui")]
+pub struct Window {
+    #[template_child]
+    pub main_button: TemplateChild<Button>,
+    #[template_child]
+    pub image2: TemplateChild<Image>,
+}
+// ANCHOR_END: object
+
+// ANCHOR: subclass
+// The central trait for subclassing a GObject
+#[glib::object_subclass]
+impl ObjectSubclass for Window {
+    // `NAME` needs to match `class` attribute of template
+    const NAME: &'static str = "MainAppWindow";
+    type Type = super::Window;
+    type ParentType = gtk::ApplicationWindow;
+
+    fn class_init(klass: &mut Self::Class) {
+        klass.bind_template();
+    }
+
+    fn instance_init(obj: &InitializingObject<Self>) {
+        obj.init_template();
+    }
+}
+// ANCHOR_END: subclass
+
+// ANCHOR: object_impl
+// Trait shared by all GObjects
+impl ObjectImpl for Window {
+    fn constructed(&self) {
+        // Call "constructed" on parent
+        self.parent_constructed();
+
+        // Connect to "clicked" signal of `button`
+        self.main_button.connect_clicked(move |button| {
+            // Set the label to "Hello World!" after the button has been clicked on
+            button.set_label("Hello World!");
+            button.remove_css_class("destructive-action");
+            button.add_css_class("suggested-action");
+            
+        });
+        let image_path: Option<&str>  = "../../resources/img/prueba.png";
+        self.image2.set_from_resource(image_path);
+    }
+}
+// ANCHOR_END: object_impl
+
+// Trait shared by all widgets
+impl WidgetImpl for Window {}
+
+// Trait shared by all windows
+impl WindowImpl for Window {}
+
+// Trait shared by all application windows
+impl ApplicationWindowImpl for Window {}
