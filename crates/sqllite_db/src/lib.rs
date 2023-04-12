9292da9a0773207292099c8a6bab06e57c371bc0
alex <axel6043@gmail.com>
2023-04-12 10:38:59 +0200

    added sea-orm

--- crates/sqllite_db/src/lib.rs
+++ crates/sqllite_db/src/lib.rs
@@ -1,14 +1,44 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod entities;
+
+use sea_orm::*;
+
+const DATABASE_URL: &str = "sqlite://crates/sqllite_db/resources/pcfutbol.db";
+const DB_NAME: &str = "pcfutbol_db";
+
+
+pub async fn start_db() -> Result<DatabaseConnection, DbErr> {
+
+    let db = Database::connect(DATABASE_URL).await?;
+
+    let db = match db.get_database_backend() {
+        DbBackend::MySql => {
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("CREATE DATABASE IF NOT EXISTS `{}`;", DB_NAME),
+            ))
+            .await?;
+
+            let url = format!("{}/{}", DATABASE_URL, DB_NAME);
+            Database::connect(&url).await?
+        }
+        DbBackend::Postgres => {
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("DROP DATABASE IF EXISTS \"{}\";", DB_NAME),
+            ))
+            .await?;
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("CREATE DATABASE \"{}\";", DB_NAME),
+            ))
+            .await?;
+
+            let url = format!("{}/{}", DATABASE_URL, DB_NAME);
+            Database::connect(&url).await?
+        }
+        DbBackend::Sqlite => db,
+    };
+
+    Ok(db)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use utils::GenericError;
-use gtk_ui as ui;
+use tauri_next_ui as ui;
+use sqllite_db as db;
 
-fn main() -> Result<(),GenericError> {
+#[tokio::main]
+async fn main() -> Result<(),GenericError> {
     
-    ui::start_gtk_ui()?;
+    //ui::start_gtk_ui()?;
+    let db = match db::start_db().await {
+           Ok(db) => db,
+           Err(err) => panic!("{}", err),
+    };
 
     Ok(())
 }
